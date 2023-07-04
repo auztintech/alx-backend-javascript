f4a30e105ca5f1bf26d7dde28ec72c241d9e45ad
Edwin Augustine <auztintech@gmail.com>
2023-07-04 15:42:57 +0100

    initial commit

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,7 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`))
+    .catch(() => console.log('Signup system offline'));
+}
