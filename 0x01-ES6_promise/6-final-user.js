ccbd1023fee1daacb31e22ac35a7d1d76e43af18
Edwin Augustine <auztintech@gmail.com>
2023-07-04 16:00:16 +0100

    initial commit

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+   return Promise.allSettled([userPromise, photoPromise]).then((results) => { //eslint-disable-line
+    return results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
+      }
+      return {
+        status: 'rejected',
+        value: result.reason,
+      };
+    });
+  });
+} // eslint-disable-line
