ad7671ba00eb636d4ad27bccc8ad3e1a1247e36f
Edwin Augustine <auztintech@gmail.com>
2023-07-04 22:38:00 +0100

    initail commit

---
+++ 0x00-ES6_basic/0-main.js
@@ -0,0 +1,3 @@
+import { taskFirst, taskNext } from './0-constants.js'; // eslint-disable-line
+
+console.log(`${taskFirst()} ${taskNext()}`);
---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,9 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+  if (trueOrFalse) {
+      const task = true; // eslint-disable-line
+      const task2 = false; // eslint-disable-line
+} // eslint-disable-line
+  return [task, task2];
+} // eslint-disable-line
